5e5689589d7ccbbd733136794f6d38272c817ae5
Gregory Ashton <gregory.ashton@ligo.org>
2023-04-13 09:40:36 +0000

    More minor updates

--- jupyterhub/setup_nginx.sh
+++ jupyterhub/setup_nginx.sh
@@ -1,3 +1,7 @@
 # See https://professorkazarinoff.github.io/jupyterhub-engr101/nginx_config/
 sudo apt install nginx -y
 
+# See https://www.server-world.info/en/note?os=Ubuntu_20.04&p=nginx&f=4 to setup userdir
+
+# Setup site through google domains
+
