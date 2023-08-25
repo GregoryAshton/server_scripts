40e93be2b97ef66a09a1f54269b98d0cf73e9c4f
Gregory Ashton <gregory.ashton@ligo.org>
2023-08-25 15:54:09 +0000

    Update the installation scripts

--- jupyterhub/create_conda_sitewide.sh
+++ jupyterhub/create_conda_sitewide.sh
@@ -2,5 +2,5 @@
 sudo mkdir -p /opt/conda/envs/
 
 # Set up the default python installation with some standard packages
-sudo /opt/conda/bin/conda create --prefix /opt/conda/envs/python3.9 python=3.9 ipykernel ipython numpy matplotlib pandas scipy ipympl scikit-learn  -y
-sudo /opt/conda/envs/python3.9/bin/python -m ipykernel install --prefix /usr/local/ --name 'python3.9' --display-name "Python 3.9 (default)"
+sudo /opt/conda/bin/conda create --prefix /opt/conda/envs/python3.9_data_science python=3.9 ipykernel ipython numpy matplotlib pandas scipy ipympl scikit-learn ipywidgets iminuit plotly pymc sympy -y
+sudo /opt/conda/envs/python3.9_data_science/bin/python -m ipykernel install --prefix /usr/local/ --name 'python3.9_data_science' --display-name "Python 3.9 Data Science (default)"
--- software/install.sh
+++ software/install.sh
@@ -1,1 +1,10 @@
 sudo apt-get install tmux -y
+sudo apt-get install texlive-latex-base -y
+sudo apt-get install texlive-fonts-recommended -y
+sudo apt-get install texlive-fonts-extra -y
+sudo apt-get install texlive-latex-extra -y
+sudo apt-get install texlive-xetex texlive-plain-generic -y
+sudo apt-get install pandoc -y
+sudo apt-get install gcc -y
+sudo apt-get install gedit -y
+
